a1226c0c694e98263148aae401224378fdf7712c
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-03-24 12:38:18 +0500

    All CRUD operations constructed

--- Data/GameService.cs
+++ Data/GameService.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorServerCRUD.Data;
@@ -5,17 +6,83 @@ namespace BlazorServerCRUD.Data;
 public class GameService : IGameService
 {
     private readonly ApplicationDbContext _context;
+    private readonly NavigationManager _navigationManager;
 
     public List<Game> Games { get; set; } = new();
 
-    public GameService(ApplicationDbContext context)
+    public GameService(ApplicationDbContext context, NavigationManager navigationManager)
     {
         _context = context;
+        _navigationManager = navigationManager;
         _context.Database.EnsureCreated();
     }
 
     public async Task LoadGames()
     {
         Games = await _context.Games.ToListAsync();
+    }
+
+    public async Task<Game> GetSingleGame(int id)
+    {
+        var game = await _context.Games.FirstOrDefaultAsync(g => g.Id == id);
+        if (game is null)
+            throw new Exception("No game here, :/");
+
+        return game;
+    }
+
+    public async Task CreateGame(Game game)
+    {
+        try
+        {
+            await _context.AddAsync(game);
+            await _context.SaveChangesAsync();
+
+            _navigationManager.NavigateTo("videogames");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Adding game is failed :/\n{ex.Message}");
+        }
+    }
+
+    public async Task UpdateGame(Game game, int id)
+    {
+        var dbGame = await _context.Games.FirstOrDefaultAsync(g => g.Id == id);
+        if (dbGame is null)
+            throw new Exception("No game here, :/");
+
+        dbGame.Name = game.Name ?? dbGame.Name;
+        dbGame.Developer = game.Developer ?? dbGame.Developer;
+        dbGame.Release = game.Release ?? dbGame.Release;
+
+        try
+        {
+            _context.Games.Update(dbGame);
+            await _context.SaveChangesAsync();
+            _navigationManager.NavigateTo("videogames");
+        }
+        catch(Exception ex)
+        {
+            throw new Exception($"Updating game is failed :/\n{ex.Message}");
+        }
+    }
+
+    public async Task DeleteGame(int id)
+    {
+        var game = await _context.Games.FirstOrDefaultAsync(g => g.Id == id);
+        if (game is null)
+            throw new Exception("No game here, :/");
+
+        try
+        {
+            _context.Games.Remove(game);
+            await _context.SaveChangesAsync();
+            _navigationManager.NavigateTo("videogames");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Deleting game is failed :/\n{ex.Message}");
+        }
     }
 }
--- Data/IGameService.cs
+++ Data/IGameService.cs
@@ -3,6 +3,9 @@ namespace BlazorServerCRUD.Data;
 public interface IGameService
 {
     List<Game> Games { get; set; }
-
     Task LoadGames();
+    Task<Game> GetSingleGame(int id);
+    Task CreateGame(Game game);
+    Task UpdateGame(Game game, int id);
+    Task DeleteGame(int id);
 }
